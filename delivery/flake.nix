2ca76e4bcf0773f1bdd8b13d620a31e394008d47
Pierrot LC <pierrotlc@proton.me>
2025-05-12 16:05:36 +0200

    ci(delivery): using buildInputs

--- delivery/flake.nix
+++ delivery/flake.nix
@@ -12,14 +12,16 @@
     system = "x86_64-linux";
     pkgs = import nixpkgs {inherit system;};
 
+    buildInputs = [
+      pkgs.erlang_nox
+      pkgs.gleam
+      pkgs.just
+      pkgs.rebar3
+    ];
+
     shell = pkgs.mkShell {
       name = "delivery";
-      packages = [
-        pkgs.erlang_nox
-        pkgs.gleam
-        pkgs.just
-        pkgs.rebar3
-      ];
+      inherit buildInputs;
     };
   in {
     devShells.${system}.default = shell;
