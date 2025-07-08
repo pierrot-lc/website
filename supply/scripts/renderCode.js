b41d61e06d635f91fb09a5d48d8535eea58b325c
Pierrot LC <pierrotlc@proton.me>
2025-07-09 01:29:11 +0200

    feat(highlight-js): plugin to mark lines

--- supply/scripts/renderCode.js
+++ supply/scripts/renderCode.js
@@ -1,1 +1,14 @@
 hljs.highlightAll();
+
+// Replace lines marked by a comment 'mark-line' with encapsulated <mark>line</mark>.
+// From https://github.com/highlightjs/highlight.js/issues/740#issuecomment-1298487876
+hljs.addPlugin({
+    'after:highlightElement': ({ el, result, text }) => {
+        let html = el.innerHTML;
+        let markedHtml = html.replace(/^(\s*)(.+?)\s*<span class="hljs-comment">.*?\bmark-line\b.*?<\/span>$/mg, '$1<mark>$2</mark>');
+
+        if (html != markedHtml) {
+            el.innerHTML = markedHtml;
+        }
+    }
+});
